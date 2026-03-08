16587c79461764366bab18eeeba8517765607f1b
Sunil Raj Ramachandran <rajsunil031@gmail.com>
2026-03-09 00:16:23 +0530

    Added diff() function

--- src/main/java/MyPkg/MyCalc.java
+++ src/main/java/MyPkg/MyCalc.java
@@ -5,10 +5,15 @@ public class MyCalc {
 	{
 		return(a + b);
 	}
+	public int diff(int a , int b)
+	{
+		return(a - b);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MyCalc calc = new MyCalc();
 		System.out.println("Sum is : " + calc.sum(20 , 10));
+		System.out.println("Difference is : " + calc.diff(20 ,10));
 	}
 
 }
