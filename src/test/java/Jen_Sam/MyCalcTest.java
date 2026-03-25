1a29b7070e4973a44eb485a18aa916258d893654
Sunil Raj Ramachandran <rajsunil031@gmail.com>
2026-03-26 00:51:52 +0530

    Added Test case file for validation using Junit and also added dependency for Maven related code in pom.xml

---
+++ src/test/java/Jen_Sam/MyCalcTest.java
@@ -0,0 +1,17 @@
+package Jen_Sam;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import MyPkg.MyCalc;
+
+public class MyCalcTest {
+
+	@Test
+	public void test() {
+		MyCalc calc = new MyCalc();
+		assertEquals (30 , calc.sum(20,10));
+	}
+
+}
